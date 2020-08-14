e1d4fb0bf0f2de1c6d61e26d8a57c3a7d5f931a2
aapostolou <63057992+aapostolou@users.noreply.github.com>
2020-08-14 15:37:09 +0300

    Update server.js

--- WebServer & Client/server.js
+++ WebServer & Client/server.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
-  console.log("Socket connected");
+    console.log("Socket connected");
 
-  socket.on("disconnect", () => {
-    console.log("Socket disconnected");
-  });
+    socket.on("disconnect", () => {
+        console.log("Socket disconnected");
+    });
 });
 
 server.listen(port, () => {
-  console.log(`Server listening on port: ${port}`);
+    console.log(`Server listening on port: ${port}`);
 });
