884fa117bcfc24b9b5b817158f4a5bdb534d1b06
aapostolou <63057992+aapostolou@users.noreply.github.com>
2020-08-14 15:37:34 +0300

    Update server.js

--- WebServer & Client/server.js
+++ WebServer & Client/server.js
@@ -6,7 +6,7 @@ const io = require("socket.io")(http);
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 io.on("connection", (socket) => {
