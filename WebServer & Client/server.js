049422337dca3fc9b3821bdc1b12823457beb707
aapostolou <63057992+aapostolou@users.noreply.github.com>
2020-11-29 23:04:33 +0200

    Update server.js

--- WebServer & Client/server.js
+++ WebServer & Client/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(require("http"));
+const io = require("socket.io")(server);
 
 const port = 1994;
 
